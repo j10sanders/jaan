be1784d5694c8fcc58201e2ec415a40ac0b7aa81
j10sanders <jonsandersss@gmail.com>
2022-02-27 14:33:20 -0500

    idk man

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,16 +15,6 @@ const Menu: React.FC<MenuProps> = ({ setIsOpen }) => {
 
   return (
     <>
-      {/* <svg
-        className={`hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-background transform translate-x-1/2`}
-        fill="currentColor"
-        viewBox="0 0 100 100"
-        preserveAspectRatio="none"
-        aria-hidden="true"
-      >
-        <polygon points="50,0 100,0 50,100 0,100" />
-      </svg> */}
-
       <Popover>
         <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
           <nav
