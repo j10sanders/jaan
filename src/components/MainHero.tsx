1ef00a0860e8345e81f273db4306008913d9d29e
j10sanders <jonsandersss@gmail.com>
2022-02-27 14:03:55 -0500

    calc height

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -5,18 +5,18 @@ import config from '../config/index.json';
 const MainHero = () => {
   const { mainHero } = config;
   return (
-    <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-      <div className="text-center">
-        <h1 className="text-4xl tracking-tight text-gray-400 sm:text-5xl md:text-6xl pt-20">
-          <span className="block xl:inline">{mainHero.title}</span>{' '}
-          {/* <span className={`block text-primary xl:inline`}>
+    <main className="flex justify-center items-center h-[calc(100%-200px)]">
+      {/* <div className="text-center"> */}
+      <h1 className="text-4xl tracking-tight text-gray-400 sm:text-5xl md:text-6xl">
+        <span className="block xl:inline">{mainHero.title}</span>{' '}
+        {/* <span className={`block text-primary xl:inline`}>
             {mainHero.subtitle}
           </span> */}
-        </h1>
-        {/* <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+      </h1>
+      {/* <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
           {mainHero.description}
         </p> */}
-        {/* <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+      {/* <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
           <div className="rounded-md shadow">
             <a
               href={mainHero.primaryAction.href}
@@ -34,7 +34,7 @@ const MainHero = () => {
             </a>
           </div>
         </div> */}
-      </div>
+      {/* </div> */}
     </main>
   );
 };
