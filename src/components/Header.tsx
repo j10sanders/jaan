304028dc4675bfe93234e1ec99a99f07f16e9f93
j10sanders <jonsandersss@gmail.com>
2022-03-05 17:00:17 -0500

    remove logo from mobile popup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -86,10 +86,7 @@ const Menu: React.FC<MenuProps> = ({ setIsOpen }) => {
             <div
               className={`bg-gray-300	 rounded-lg shadow-md ring-1 ring-black ring-opacity-5 overflow-hidden`}
             >
-              <div className="px-5 pt-4 flex items-center justify-between">
-                <div>
-                  <img className="h-8 w-auto" src={logo} alt="" />
-                </div>
+              <div className="px-5 pt-4 flex items-center justify-end">
                 <div className="-mr-2">
                   <Popover.Button
                     className={`rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-secondary`}
